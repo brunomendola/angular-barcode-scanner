07591fbee95d11567ad8796289dad90645fd482a
Bruno Mendola <1853562+brunomendola@users.noreply.github.com>
2019-12-09 18:01:24 +0100

    If available, use event.key to get the pressed key character

--- src/barcode-scanner.directive.js
+++ src/barcode-scanner.directive.js
@@ -12,18 +12,19 @@ export default ($window) => {
         link: (scope, element) => {
             let buffer = '';
             const handler = (e) => {
-                const keycode = e.keyCode || e.which;
+                const key = e.key;
+                const keyCode = e.which || e.keyCode;
                 const triggerChar = parseInt(scope.triggerChar, 10);
                 const separatorChar = parseInt(scope.separatorChar, 10);
 
-                if (keycode === triggerChar) {
+                if (keyCode === triggerChar) {
                     scope.triggerCallback();
                     e.preventDefault();
                 } else {
-                    const valid = keycode >= 32 && keycode <= 255;
-                    if (valid === true && keycode !== separatorChar) {
-                        buffer += String.fromCharCode(keycode);
-                    } else if (keycode === separatorChar) {
+                    const valid = keyCode >= 32 && keyCode <= 255;
+                    if (valid === true && keyCode !== separatorChar) {
+                        buffer += key || String.fromCharCode(keyCode);
+                    } else if (keyCode === separatorChar) {
                         if (buffer.length > 0) {
                             scope.scanCallback(buffer);
                         }
